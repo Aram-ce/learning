ce3e37cd23296a1733ca578a53aeca40df207828
Aram_ce <aramamanifar8@gmail.com>
2024-10-12 01:36:41 -0700

    7: add if condition

--- learning/Program.cs
+++ learning/Program.cs
@@ -3,5 +3,13 @@ using learning;
 
 //Sum.Sum4Number();
 //FOR.SumByFor();
-ForZoj.ForByZoj2();
+//ForZoj.ForByZoj2();
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+    Console.WriteLine(" number is even");
+else
+    Console.WriteLine("number is odd");
+
+
 
