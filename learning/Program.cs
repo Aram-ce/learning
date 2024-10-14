1d35f61e66b5c94e43589f7c997e6215006e3f3c
Aram_ce <aramamanifar8@gmail.com>
2024-10-14 01:00:57 -0700

    10

--- learning/Program.cs
+++ learning/Program.cs
@@ -10,11 +10,16 @@ Console.WriteLine("day:");
 d = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("month:");
 m = Convert.ToInt32(Console.ReadLine());
-if (m<=6)
-    Console.WriteLine(((m-1)*31)+d);
-else 
+if (m <= 6)
+  Console.WriteLine(((m - 1) * 31) + d);
+else
 
-    Console.WriteLine(((m-1)*30+d));
+    Console.WriteLine(((d+(30*(m - 7)))+6 * 31));
+        
+
+
+
+
 
 
 
