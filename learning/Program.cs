72e3593cde8c1541bcb8202d79315bced1157371
Aram_ce <aramamanifar8@gmail.com>
2024-10-21 23:43:30 -0700

    12

--- learning/Program.cs
+++ learning/Program.cs
@@ -1,21 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using learning;
 using System.ComponentModel.Design;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 //Sum.Sum4Number();
 //FOR.SumByFor();
 //ForZoj.ForByZoj2();
 
-Console.Write("یک عدد وارد کنید: ");
-int number = int.Parse(Console.ReadLine());
-int i = 0;
+//Console.Write(" عدد 1 وارد کنید: ");
+//int number = int.Parse(Console.ReadLine());
+//int i = 0;
+//while (i<number)
 
-while (i<= number)
+//{
+//    Console.WriteLine(i+=2);
+//}
+double a;
+Console.WriteLine("adad ra vared konid:");
+a = Convert.ToInt32(Console.ReadLine());
+double b;
+Console.WriteLine("adad ra vared konid:");
+b = Convert.ToInt32(Console.ReadLine());
+double result = Math.Pow(a, b);
 
-{
-    Console.WriteLine(i++);
-    
-}
+Console.WriteLine($"{a} be tavan {b} barabar ast ba {result}");
 
 
 
